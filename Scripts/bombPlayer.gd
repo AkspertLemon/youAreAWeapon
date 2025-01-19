467546b32f7ba6052299ff3161ccaf2112d2b7a4
AkspertLemon <abdullahfarhantcs@gmail.com>
2025-01-19 13:02:58 +0500

    Finalizing Player
    Added Sounds, player sprite complete, custom projectile numbers and rotation.
    SFX also for players.

--- Scripts/bombPlayer.gd
+++ Scripts/bombPlayer.gd
@@ -14,23 +14,33 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var input = Vector2.ZERO
+	var spin = 0
 	input.x = Input.get_axis("move_a","move_d")
 	input.y = Input.get_axis("move_w",'move_s')
+	spin = Input.get_axis("rot_q","rot_e")
 	apply_central_force(input*delta*10000)
+	apply_torque(spin*2000)
+	if  not $Clock_ticker.playing && not $Explode_snd.playing:$Clock_ticker.play(0)
 	
 	
 func _on_bomb_timer_timeout() -> void:
 	var tween := create_tween().bind_node(self).set_trans(Tween.TRANS_QUART).set_parallel(false)
 	tween.tween_property($MeshInstance2D, "modulate", Color.CRIMSON, 2)
 	tween.tween_property($MeshInstance2D, "modulate", Color.DARK_TURQUOISE, 0)
+	$Timer.start()
 	
 	
+@export var projec_num = 100
 func explode() -> void:
-	for n in range(4):
+	$Clock_ticker.stop()
+	$Explode_snd.play(0)
+	var rot_offset = 2*PI/projec_num
+	for n in range(projec_num):
 		var instance = projectile.instantiate()
-		instance.dir = rotation+(PI*(n-1)*0.5)
+		instance.dir = rotation+((n-1)*rot_offset)
 		instance.spawnPos = global_position
-		instance.spawnRot = rotation+(PI*(n-1)*0.5)
+		instance.spawnRot = rotation+((n-1)*rot_offset)
+		instance.zdex = z_index-1
 		main.add_child.call_deferred(instance)
 
 
--- Scripts/tri_projectile.gd
+++ Scripts/tri_projectile.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
-var SPEED = 200
+var SPEED = 600
 var dir:float
 var spawnPos:Vector2
 var spawnRot:float
-
+var zdex:int
 func _ready() -> void:
+	z_index=zdex
 	global_position=spawnPos
 	global_rotation=spawnRot
 	#$Sprite2D.rotation=spawnRot
